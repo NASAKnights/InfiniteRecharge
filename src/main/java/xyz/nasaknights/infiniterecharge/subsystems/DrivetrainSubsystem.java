13e7111789ef23867402e5b277fd7149fbd60691
Bradley Hooten <hello@bradleyh.me>
2020-01-06 19:14:00 -0500

    Initial

---
+++ src/main/java/xyz/nasaknights/infiniterecharge/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,14 @@
+package xyz.nasaknights.infiniterecharge.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+    public DrivetrainSubsystem() {
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+}
---
+++ src/main/java/xyz/nasaknights/infiniterecharge/util/controller/PS4ControllerMappings.java
@@ -0,0 +1,5 @@
+package xyz.nasaknights.infiniterecharge.util.controller;
+
+public enum PS4ControllerMappings {
+
+}
