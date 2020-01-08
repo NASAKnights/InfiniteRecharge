d949bb18abbdc79b591eaa547785e1a8023f4dd2
LeviWalker <levi.j.walker04@gmail.com>
2020-01-08 16:46:52 -0500

    added controller mappings; IDs need  verification

--- src/main/java/xyz/nasaknights/infiniterecharge/util/controller/PS4ControllerMappings.java
+++ src/main/java/xyz/nasaknights/infiniterecharge/util/controller/PS4ControllerMappings.java
@@ -2,4 +2,36 @@ package xyz.nasaknights.infiniterecharge.util.controller;
 
 public enum PS4ControllerMappings {
 
+    //TODO Verify IDs
+
+    //buttons
+    X(1),
+    CIRCLE(2),
+    SQUARE(3),
+    TRIANGLE(4),
+    RIGHT_BUMPER(6),
+    LEFT_BUMPER(5),
+    SHARE(7),
+    OPTIONS(8),
+    LEFT_JOYSTICK(9),
+    RIGHT_JOYSTICK(10),
+
+    //axes
+    LEFT_X_AXIS(0),
+    LEFT_Y_AXIS(1),
+    LEFT_TRIGGER(2),
+    RIGHT_TRIGGER(3),
+    RIGHT_X_AXIS(4),
+    RIGHT_Y_AXIS(5);
+
+    private int id;
+        
+    PS4ControllerMappings(int id) {
+        this.id = id;
+    }
+
+    public int getID() {
+        return this.id;
+    }
+
 }
---
+++ src/main/java/xyz/nasaknights/infiniterecharge/util/controller/XBoxControllerMappings.java
@@ -0,0 +1,34 @@
+package xyz.nasaknights.infiniterecharge.util.controller;
+
+public enum XBoxControllerMappings {
+
+    //TODO get real ID numbers
+
+    //axes
+    LEFT_X_AXIS(0),
+    LEFT_Y_AXIS(0),
+    LEFT_TRIGGER(0),
+    RIGHT_TRIGGER(0),
+    RIGHT_X_AXIS(0),
+    RIGHT_Y_AXIS(0),
+
+    //buttons
+    A(0),
+    B(0),
+    X(0),
+    Y(0),
+    LEFT_BUMPER(0),
+    RIGHT_BUMPER(0),
+    BACK(0),
+    START(0);
+
+    private int id;
+
+    XBoxControllerMappings(int id) {
+        this.id = id;
+    }
+
+    public int getID() {
+        return id;
+    }
+}
