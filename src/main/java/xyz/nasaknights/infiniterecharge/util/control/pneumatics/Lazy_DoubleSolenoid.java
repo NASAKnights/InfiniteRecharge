5511029d3e7baa74e350e0a3611089633760912e
LeviWalker <levi.j.walker04@gmail.com>
2020-01-09 19:55:36 -0500

    Added lazy motor controllers and pneumatics

---
+++ src/main/java/xyz/nasaknights/infiniterecharge/util/control/pneumatics/Lazy_DoubleSolenoid.java
@@ -0,0 +1,40 @@
+package xyz.nasaknights.infiniterecharge.util.control.pneumatics;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+/**
+ * A lazy implementation of the DoubleSolenoid class to reduce CAN overhead.
+ */
+public class Lazy_DoubleSolenoid extends DoubleSolenoid {
+
+    private Value lastValue;
+
+    /**
+     * Creates an instance of a Lazy_DoubleSolenoid
+     * @param forwardChannel The forward channel number on the PCM (0..7).
+     * @param reverseChannel The reverse channel number on the PCM (0..7).
+     */
+    public Lazy_DoubleSolenoid(int forwardChannel, int reverseChannel) {
+        super(forwardChannel, reverseChannel);
+    }
+
+    /**
+     * Creates an instance of a Lazy_DoubleSolenoid
+     * @param moduleNumber The module number of the solenoid to module
+     * @param forwardChannel The forward channel number on the PCM (0..7).
+     * @param reverseChannel The reverse channel number on the PCM (0..7).
+     */
+    public Lazy_DoubleSolenoid(int moduleNumber, int forwardChannel, int reverseChannel) {
+        super(moduleNumber, forwardChannel, reverseChannel);
+    }
+
+    /**
+     * Sets the Value of the Lazy_DoubleSolenoid
+     * @param value The value to set the Lazy_DoubleSolenoid
+     */
+    @Override
+    public void set(Value value) {
+        if (lastValue != value) super.set(value);
+    }
+
+}
